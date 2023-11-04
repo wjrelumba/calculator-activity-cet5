f7398ffe9a4ad32751dc6be2bf392e8e95358469
wjrelumba <williamjamesrelumba@iskolarngbayan.pup.edu.ph>
2023-11-04 15:23:12 +0800

    Fixed decimal adding of 0.1 and 0.2

--- functions.js
+++ functions.js
@@ -20,6 +20,11 @@ var subtractionEvent = false;
 var multiplicationEvent = false;
 var divisionEvent = false;
 
+var decimalPoint = false;
+
+var firstMultiplier = 1;
+var secondMultiplier = 1;
+
 var firstOp = 0;
 var secondOp = 0;
 
@@ -27,17 +32,34 @@ calcuScreen.value = 0;
 
 Array.from(buttonClicked).forEach(btn => {
     btn.addEventListener("click", (event) => {
+        var valueClicked = event.target.value;
         if(calculated == true){
             calcuScreen.value = "0";
             calculated = false;
-            calcuScreen.value = event.target.value;
+            calcuScreen.value = valueClicked;
         }
         else{
             if(calcuScreen.value == "0"){
-                calcuScreen.value = event.target.value;
+                if(valueClicked == "."){
+                    if(decimalPoint == false){
+                        calcuScreen.value += valueClicked;
+                        decimalPoint = true;
+                    }
+                }
+                else{
+                    calcuScreen.value = valueClicked;
+                }
             }
             else{
-                calcuScreen.value += event.target.value;
+                if(valueClicked == "."){
+                    if(decimalPoint == false){
+                        calcuScreen.value += valueClicked;
+                        decimalPoint = true;
+                    }
+                }
+                else{
+                    calcuScreen.value += valueClicked;
+                }
             }
         }
     })
@@ -45,20 +67,89 @@ Array.from(buttonClicked).forEach(btn => {
 
 clearBtn.addEventListener("click", () => {
     calcuScreen.value = "0";
+    decimalPoint = false;
 });
 
 plusBtn.addEventListener("click", (event) => {
+    firstMultiplier = 1;
+    secondMultiplier = 1;
     additionEvent = true;
     firstNumberInput = true;
     if(firstNumberInput == true && secondNumberInput == false){
-        firstOp = parseFloat(calcuScreen.value);
-        calcuScreen.value = "";
-        secondNumberInput = true;
+        if(decimalPoint == true){
+            firstMultiplier = numberTraversal(calcuScreen.value);
+            firstOp = parseFloat(calcuScreen.value);
+            calcuScreen.value = "0";
+            decimalPoint = false;
+            secondNumberInput = true;
+        }
+        else{
+            firstOp = parseFloat(calcuScreen.value);
+            calcuScreen.value = "0";
+            decimalPoint = false;
+            secondNumberInput = true;
+        }
     }
     else if(firstNumberInput == true && secondNumberInput == true){
-        secondOp = parseFloat(calcuScreen.value);
-        calcuScreen.value = firstOp + secondOp;
-        secondNumberInput = false;
+        if(decimalPoint == true){
+            secondMultiplier = numberTraversal(calcuScreen.value);
+            secondOp = parseFloat(calcuScreen.value);
+            if(firstMultiplier > secondMultiplier){
+                secondMultiplier = firstMultiplier;
+                firstOp *= firstMultiplier;
+                secondOp *= firstMultiplier;
+                var calculation = firstOp + secondOp;
+                calculation /= firstMultiplier;
+                calcuScreen.value = calculation;
+                secondNumberInput = false;
+            }
+            else if(secondMultiplier > firstMultiplier){
+                firstMultiplier = secondMultiplier;
+                firstOp *= firstMultiplier;
+                secondOp *= firstMultiplier;
+                var calculation = firstOp + secondOp;
+                calculation /= firstMultiplier;
+                calcuScreen.value = calculation;
+                secondNumberInput = false;
+            }
+            else{
+                firstOp *= firstMultiplier;
+                secondOp *= firstMultiplier;
+                var calculation = firstOp + secondOp;
+                calculation /= firstMultiplier;
+                calcuScreen.value = calculation;
+                secondNumberInput = false;
+            }
+        }
+        else{
+            secondOp = parseFloat(calcuScreen.value);
+            if(firstMultiplier > secondMultiplier){
+                secondMultiplier = firstMultiplier;
+                firstOp *= firstMultiplier;
+                secondOp *= firstMultiplier;
+                var calculation = firstOp + secondOp;
+                calculation /= firstMultiplier;
+                calcuScreen.value = calculation;
+                secondNumberInput = false;
+            }
+            else if(secondMultiplier > firstMultiplier){
+                firstMultiplier = secondMultiplier;
+                firstOp *= firstMultiplier;
+                secondOp *= firstMultiplier;
+                var calculation = firstOp + secondOp;
+                calculation /= firstMultiplier;
+                calcuScreen.value = calculation;
+                secondNumberInput = false;
+            }
+            else{
+                firstOp *= firstMultiplier;
+                secondOp *= firstMultiplier;
+                var calculation = firstOp + secondOp;
+                calculation /= firstMultiplier;
+                calcuScreen.value = calculation;
+                secondNumberInput = false;
+            }
+        }
     }
 })
 
@@ -67,7 +158,8 @@ minusBtn.addEventListener("click", (event) => {
     firstNumberInput = true;
     if(firstNumberInput == true && secondNumberInput == false){
         firstOp = parseFloat(calcuScreen.value);
-        calcuScreen.value = "";
+        calcuScreen.value = "0";
+        decimalPoint = false;
         secondNumberInput = true;
     }
     else if(firstNumberInput == true && secondNumberInput == true){
@@ -82,7 +174,8 @@ divideBtn.addEventListener("click", (event) => {
     firstNumberInput = true;
     if(firstNumberInput == true && secondNumberInput == false){
         firstOp = parseFloat(calcuScreen.value);
-        calcuScreen.value = "";
+        calcuScreen.value = "0";
+        decimalPoint = false;
         secondNumberInput = true;
     }
     else if(firstNumberInput == true && secondNumberInput == true){
@@ -98,6 +191,7 @@ multiplyBtn.addEventListener("click", (event) => {
     if(firstNumberInput == true && secondNumberInput == false){
         firstOp = parseFloat(calcuScreen.value);
         calcuScreen.value = "";
+        decimalPoint = false;
         secondNumberInput = true;
     }
     else if(firstNumberInput == true && secondNumberInput == true){
@@ -116,6 +210,7 @@ equalBtn.addEventListener("click", () => {
             firstNumberInput = false;
             secondNumberInput = false;
             additionEvent = false;
+            decimalPoint = false;
         }
         else if(subtractionEvent == true){
             calcuScreen.value = firstOp - secondOp;
@@ -123,6 +218,7 @@ equalBtn.addEventListener("click", () => {
             firstNumberInput = false;
             secondNumberInput = false;
             subtractionEvent = false;
+            decimalPoint = false;
         }
         else if(divisionEvent == true){
             calcuScreen.value = firstOp / secondOp;
@@ -130,6 +226,7 @@ equalBtn.addEventListener("click", () => {
             firstNumberInput = false;
             secondNumberInput = false;
             divisionEvent = false;
+            decimalPoint = false;
         }
         else if(multiplicationEvent == true) {
             calcuScreen.value = firstOp * secondOp;
@@ -137,6 +234,28 @@ equalBtn.addEventListener("click", () => {
             firstNumberInput = false;
             secondNumberInput = false;
             multiplicationEvent = false;
+            decimalPoint = false;
         }
     }
 })
+
+const numberTraversal = (numberValue) => {
+    for(let i = 0; i < numberValue.length; i++) {
+        if(numberValue[i] == "."){
+            var multiplier = "1";
+            var multiplierNumber = 0;
+            var lengthOfNumber = numberValue.length - (i+1);
+            for(let i = 0; i < lengthOfNumber; i++){
+                multiplier += "0";
+                multiplierNumber = parseInt(multiplier);
+            }
+            numberValue *= multiplierNumber;
+            alert(numberValue);
+            // var wholeNumbers = (numberValue).slice(0, i);
+            // var decimalNumbers = (numberValue).slice(i+1, numberValue.length);
+            // alert("Whole numbers: " + wholeNumbers + " Decimal Numbers: " + decimalNumbers);
+        }
+    }
+    console.log(multiplier);
+    return multiplier;
+}
